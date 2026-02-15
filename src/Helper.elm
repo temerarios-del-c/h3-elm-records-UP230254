ba4f430463d7ad83a4465d2f213676f5189ac8b9
UP230254 <up230254@alumnos.upa.edu.mx>
2026-02-15 14:08:57 -0600

    Tarea

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,1 +1,107 @@
 module Helper exposing (..)
+
+import Html exposing (..)
+
+
+
+--Type annotation
+--Programas o lenguages
+
+
+langs : List { name : String, releaseYear : Int, currentVersion : String }
+langs =
+    [ { name = "elm", releaseYear = 2012, currentVersion = "0.19.1" }
+    , { name = "javascript", releaseYear = 1995, currentVersion = "ECMAScript 2025" }
+    ]
+
+
+lenguageNames : List { name : String, releaseYear : Int, currentVersion : String } -> List String
+lenguageNames list =
+    List.map .name list
+
+
+
+-- Estudiante
+
+
+users : List { name : String, uType : String }
+users =
+    [ { name = "Roberto", uType = "Student" }
+    , { name = "Mitsiu", uType = "Professor" }
+    ]
+
+
+onlyStudents : List { name : String, uType : String } -> List String
+onlyStudents list =
+    List.map
+        (\user ->
+            if user.uType == "Student" then
+                user.name
+
+            else
+                ""
+        )
+        list
+
+
+type alias Videogame =
+    { title : String, releaseYear : Int, available : Bool, downloads : Int, genres : List String }
+
+
+getVideogameGenres : Videogame -> List String
+getVideogameGenres list =
+    .genres list
+
+
+games : List Videogame
+games =
+    [ { title = "Control"
+      , releaseYear = 2019
+      , available = True
+      , downloads = 1
+      , genres = [ "Action", "Shooter" ]
+      }
+    , { title = "Ocarina of time"
+      , releaseYear = 1998
+      , available = True
+      , downloads = 2
+      , genres = [ "Action", "Adventure" ]
+      }
+    ]
+
+
+getVideoGameGenres : List Videogame -> List (List String)
+getVideoGameGenres list =
+    List.map .genres list
+
+
+type alias Computer =
+    { ram : String
+    , model : String
+    , brand : String
+    , screenSize : String
+    }
+
+
+myLaptop : Computer
+myLaptop =
+    { ram = "16GB"
+    , model = "GF65 Thin 10UE"
+    , brand = "MSI"
+    , screenSize = "15.6"
+    }
+
+
+main : Html msg
+main =
+    div []
+        [ h1 [] [ text "My laptop" ]
+        , div []
+            [ ul []
+                [ li [] [ text ("Ram: " ++ myLaptop.ram) ]
+                , li [] [ text ("Modelo: " ++ myLaptop.model) ]
+                , li [] [ text ("Marca: " ++ myLaptop.brand) ]
+                , li [] [ text ("Pulgadas: " ++ myLaptop.screenSize) ]
+                ]
+            ]
+        ]
